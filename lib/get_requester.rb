604d0df81ebf08c35e2ca821e3ac796650479ae5
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-05-14 21:09:58 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,31 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
+class GetRequester
+ 
+  def initialize
+    URL = ""
+  end
+    
+ 
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  def parse_json
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program[]  
+    end
+  end
+ 
+end
+ 
+# # programs = GetPrograms.new.get_programs
+# # puts programs
+
+# programs = GetPrograms.new
+# puts programs.program_school.uniq
